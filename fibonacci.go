8383a9a30a2a3a61db57d7599138a6801cf09614
George Zalepa <george.zalepa@gmail.com>
2023-09-08 11:16:43 -0400

    Finish exercise 4

---
+++ fibonacci.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	nums := make([]int, 10)
+
+	for i := 0; i < 10; i++ {
+		if i == 0 {
+			nums[0] = i
+		} else if i == 1 {
+			nums[1] = i
+		} else {
+			nums[i] = nums[i-1] + nums[i-2]
+		}
+	}
+
+	for i := range nums {
+		fmt.Printf("%v ", nums[i])
+	}
+
+	fmt.Print("\n")
+}
