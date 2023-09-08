b3a85e380399360c03b55cd2979bd3100b232f5b
George Zalepa <george.zalepa@gmail.com>
2023-09-08 10:58:00 -0400

    Finish exercise 2

---
+++ vars.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "George"
+	var age int = 39
+	weight := 171.1
+	isMale := true
+
+	fmt.Printf("%v is %v years old and weighs %v pounds. Is he male? %v\n", name, age, weight, isMale)
+
+	fmt.Printf("%T is %T years old and weighs %T pounds. Is he male? %T\n", name, age, weight, isMale)
+}
