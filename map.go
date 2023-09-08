00934082224ef2aa90b3d9cf13b69bc49fc6f85c
George Zalepa <george.zalepa@gmail.com>
2023-09-08 13:32:22 -0400

    Finish exercise 7

---
+++ map.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	family := make(map[string]int)
+
+	// Add a few entries
+	family["George"] = 139
+	family["Megan"] = 40
+	family["Sebastian"] = 5
+	family["Pingu"] = 14
+
+	// Update a mistake
+	family["George"] = 39
+
+	// Delete non-humans
+	delete(family, "Pingu")
+
+	for key := range family {
+		fmt.Printf("family[%v] = %v\n", key, family[key])
+	}
+
+}
